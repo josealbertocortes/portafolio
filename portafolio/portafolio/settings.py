aed321d43ebaa0c0ecf8c819325049320192f0d3
jose alberto <joalcoay94@gmail.com>
2021-02-21 11:38:17 -0600

    rquerimientos-8

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
@@ -122,4 +122,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
